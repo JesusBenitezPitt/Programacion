c54d1b4e818fc889c8efaffe130e66a45fc42325
usuario <jesus.benitezpitt@iesvalleinclan.es>
2025-01-23 12:44:08 +0100

    Ejercicios Programación 23/01/25

---
+++ Tema_07/JavaApplication29/src/Ejercicios_Resueltos/CuentaCorriente.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicios_Resueltos;
+
+/**
+ *
+ * @author usuario
+ */
+public class CuentaCorriente {
+
+    String dni;
+    String titular;
+    double saldo;
+    
+    CuentaCorriente(String dni, String titular){
+        this.dni = dni;
+        this.titular = titular;
+        this.saldo = 0;
+    }
+    
+    boolean SacarDinero(double cantidad){
+        boolean operacionPosible;
+        if( saldo){
+            
+        }
+    }
+    
+}
