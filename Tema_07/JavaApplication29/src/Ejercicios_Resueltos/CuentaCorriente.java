1cf352250cca4eb15af4ca5b91f2aa150821e533
usuario <jesus.benitezpitt@iesvalleinclan.es>
2025-01-23 13:45:14 +0100

    Ejercicios Programación 23/01/25

--- Tema_07/JavaApplication29/src/Ejercicios_Resueltos/CuentaCorriente.java
+++ Tema_07/JavaApplication29/src/Ejercicios_Resueltos/CuentaCorriente.java
@@ -9,22 +9,35 @@ package Ejercicios_Resueltos;
  * @author usuario
  */
 public class CuentaCorriente {
+        String dni;
+        String titular;
+        double saldo;
 
-    String dni;
-    String titular;
-    double saldo;
-    
-    CuentaCorriente(String dni, String titular){
-        this.dni = dni;
-        this.titular = titular;
-        this.saldo = 0;
-    }
-    
-    boolean SacarDinero(double cantidad){
-        boolean operacionPosible;
-        if( saldo){
-            
+        CuentaCorriente(String dni, String titular){
+            this.dni = dni;
+            this.titular = titular;
+            this.saldo = 0;
+        }
+
+        boolean SacarDinero(double cantidad){
+            boolean operacionPosible;
+            if(saldo >= cantidad){
+                saldo -= cantidad;
+                operacionPosible = true;
+            } else {
+            System.out.println("No hay saldo suficiente.");
+            operacionPosible = false;
+            }
+            return (operacionPosible);
+        }
+        
+        void ingreso(double cantidad){
+            saldo += cantidad;
+        }
+        
+        void mostrarInformacion(){
+            System.out.println("Nombre: " + titular);
+            System.out.println("DNI: " + dni);
+            System.out.println("Saldo: " + saldo + " euros.");
         }
     }
-    
-}
---
+++ Tema_07/JavaApplication29/src/Ejercicios_Resueltos/Ejercicio_01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicios_Resueltos;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ejercicio_01 {
+    
+    public static void main(String[] args) {
+        CuentaCorriente c;
+        c = new CuentaCorriente("12345678A", "Pepe");
+        c.ingreso(1000);
+        c.SacarDinero(2000);
+        c.mostrarInformacion();
+    }
+    
+}
