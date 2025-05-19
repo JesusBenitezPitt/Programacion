4990a7eb1f76f34a0013dbe1f708c17b08e64367
usuario <jesus.benitezpitt@iesvalleinclan.es>
2025-05-19 10:35:14 +0200

    Ejercicios Aplicacion 19/+35+/25

--- Tema_12/src/Ejercicios_Resueltos/Ejercicio_14/Main.java
+++ Tema_12/src/Ejercicios_Resueltos/Ejercicio_14/Main.java
@@ -4,10 +4,14 @@
  */
 package Ejercicios_Resueltos.Ejercicio_14;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -21,12 +25,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Map<LocalDate, Set<Registro>> mapa_temperaturas = new LinkedHashMap<>();
+        mapa_temperaturas = Registro.cargarMapa("src/Ejercicios_Resueltos/Ejercicio_14/registros.dat", mapa_temperaturas);
         Set<Registro> temperaturas = new LinkedHashSet<>();
         int op;
         do{
             System.out.println("1. Nuevo registro.");
             System.out.println("2. Listar registros del dia.");
-            System.out.println("3. Mostrar Estadisticas.");
+            System.out.println("3. Mostrar Estadisticas de un dia en concreto.");
             System.out.println("4. Salir.");
             System.out.print("Introduce una opcion: ");
             op = new Scanner(System.in).nextInt();
@@ -35,21 +41,36 @@ public class Main {
                     System.out.print("Introducir Temperatura: ");
                     double temperatura = new Scanner(System.in).useLocale(Locale.US).nextDouble();
                     temperaturas.add(new Registro(temperatura));
+                    mapa_temperaturas.put(LocalDate.now(), temperaturas);
                 }
-                case 2 -> System.out.println(temperaturas);
+                case 2 -> System.out.println(mapa_temperaturas);
                 case 3 -> {
-                    Comparator<Registro> c = (Registro o1, Registro o2) -> (int) Math.signum(o1.temperatura - o2.temperatura);
-                    System.out.println("Maxima: " + Collections.max(temperaturas, c));
-                    System.out.println("Minima: " + Collections.min(temperaturas, c));
-                    double suma = 0;
-                    for(Registro r : temperaturas){
-                        suma += r.temperatura;
+                    if(!mapa_temperaturas.isEmpty()){
+                        System.out.println("Introduce el dia (dd/MM/yyyy): ");
+                        String dia = new Scanner(System.in).next();
+                        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                        LocalDate fecha = LocalDate.parse(dia, f);
+                        if(mapa_temperaturas.get(fecha) != null){
+                            Set<Registro> registros = mapa_temperaturas.get(fecha);
+                            System.out.println(registros);
+                            Comparator<Registro> c = (Registro o1, Registro o2) -> (int) Math.signum(o1.temperatura - o2.temperatura);
+                            System.out.println("Maxima: " + Collections.max(registros, c));
+                            System.out.println("Minima: " + Collections.min(registros, c));
+                            double suma = 0;
+                            for(Registro r : registros){
+                                suma += r.temperatura;
+                            }
+                            System.out.println("Media: " + suma / registros.size());
+                        } else {
+                            System.out.println("No hay registros para ese dia.");
+                        }
+                    } else {
+                        System.out.println("No hay registros guardados.");
                     }
-                    System.out.println("Media: " + suma / temperaturas.size());
                 }
                 case 4 -> {
                     System.out.println("Guardando registros...");
-                    Registro.guardarRegistros("src/Ejercicios_Resueltos/Ejercicio_14/registros", temperaturas);
+                    Registro.guardarRegistros("src/Ejercicios_Resueltos/Ejercicio_14/registros.dat", mapa_temperaturas);
                     System.out.println("Saliendo..");
                 }
             }
--- Tema_12/src/Ejercicios_Resueltos/Ejercicio_14/Registro.java
+++ Tema_12/src/Ejercicios_Resueltos/Ejercicio_14/Registro.java
@@ -4,16 +4,20 @@
  */
 package Ejercicios_Resueltos.Ejercicio_14;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.LinkedHashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -29,6 +33,7 @@ public class Registro implements Serializable {
         this.hora = LocalTime.now();
     }
     
+    @Override
     public boolean equals(Object o){
         return hora.equals(((Registro) o ).hora);
     }
@@ -39,17 +44,25 @@ public class Registro implements Serializable {
         return "Hora: " + hora.format(f) + ", temperatura: " + temperatura + "ªC\n";
     }
     
-    public static void guardarRegistros(String archivo, Set<Registro> temperaturas){
-        DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyyMMdd");
-        archivo += (LocalDate.now().format(f) + ".dat");
+    public static void guardarRegistros(String archivo, Map<LocalDate, Set<Registro>> mapa_temperaturas){
         try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(archivo))){
-            oos.writeObject(temperaturas);
+            oos.writeObject(mapa_temperaturas);
             System.out.println("Registros guardados en " + archivo);
         }catch(FileNotFoundException e){
             e.getMessage();
         }catch(IOException e){
             e.getMessage();
         }
+    }
+
+    public static Map<LocalDate, Set<Registro>> cargarMapa(String archivo, Map<LocalDate, Set<Registro>> mapa_temperaturas) {
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo))){
+            mapa_temperaturas = (Map<LocalDate, Set<Registro>>) ois.readObject();
+            System.out.println("Registros cargados");
+        }catch(IOException | ClassNotFoundException e){
+            e.getMessage();
+        }
+        return mapa_temperaturas;
     }
     
 }
